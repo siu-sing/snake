99e73f42aed5b8f228561514df4747a07ff35c35
siu-sing <lai.siusing@gmail.com>
2020-06-20 16:00:10 +0800

    added start game interface, user controls and OOB rules

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 //Size of game area
 const gridSize = 50;
 //time taken for each refresh in ms
 const clock = 100;
 
 //Set up game board
-let setUpGameBoard = function(){
+let setUpGameBoard = function () {
     for (let i = 0; i < gridSize; i++) {
         let row = document.createElement("div");
         row.classList.add("game-row");
@@ -18,48 +17,141 @@ let setUpGameBoard = function(){
         gb.appendChild(row);
     }
 }
-
 setUpGameBoard();
 
 //Snake starting variables
-let start_i = Math.floor(gridSize/2)
-let start_j = Math.floor(gridSize/4)
+let start_i = Math.floor(gridSize / 2)
+let start_j = Math.floor(gridSize / 4)
 
 let snake = {
-    position:[start_i,start_j],
-    direction:"e",
-    move: function(){
-        switch(this.direction){
+    position: [start_i, start_j],
+    direction: "e",
+    //Function that updates the position of the snake based on its current direction
+    move: function () {
+        switch (this.direction) {
             case "e":
                 this.position[1]++;
+                break;
+            case "n":
+                this.position[0]--;
+                break;
+            case "w":
+                this.position[1]--;
+                break;
+            case "s":
+                this.position[0]++;
                 break;
         }
     }
 };
 
+//Check if snake is out of bounds
+let isSnakeOOB = function(){
+    if (snake.position[0] > gridSize - 1 //east border
+        || snake.position[0] < 0 //west border
+        || snake.position[1] > gridSize - 1 //south border
+        || snake.position[1] < 0 //north border
+    ){
+        return true;
+    } else {
+        return false;
+    }
+        
+}
+
 //Get square from coordinates
-let getSquareNode = function(i,j){
+let getSquareNode = function (i, j) {
     return document.getElementById("game-board").children[i].children[j]
 }
 
 //Set snake
-let n = getSquareNode(snake.position[0],snake.position[1]);
-n.style.backgroundColor = "red";
+let n = getSquareNode(snake.position[0], snake.position[1]);
+n.style.backgroundColor = "black";
 
 //Function that refreshes and updates snake position every clock
-let moveSnake = function(){
+let moveSnake = function () {
     let interval = setInterval(moveSnake, clock);
-    function moveSnake() {       
-        //If snake position exceeds right border
-        if(snake.position[1]>gridSize-1){
+
+    function moveSnake() {
+        console.log("snake coord:"+snake.position)
+        //If snake out of bounds
+        if (isSnakeOOB()) {
+            console.log("snake OOB");
+            clearInterval(interval);
             return;
         } else {
-            console.log(snake.position[1])
-            getSquareNode(snake.position[0],snake.position[1]).style.backgroundColor = "transparent";
+            //Move the snake
+            getSquareNode(snake.position[0], snake.position[1]).style.backgroundColor = "transparent";
             snake.move();
-            getSquareNode(snake.position[0],snake.position[1]).style.backgroundColor = "red";
+            getSquareNode(snake.position[0], snake.position[1]).style.backgroundColor = "black";
         }
     }
 }
 
-moveSnake();
+//Get middle square
+let startDisp = getSquareNode(start_i - 3, start_j * 2);
+//Set start display
+let setStartDisplay = function () {    
+    let dispP = document.createElement("p");
+    //Set styles
+    startDisp.style.lineHeight = "0";
+    startDisp.style.position = "relative";
+    startDisp.appendChild(dispP);
+    dispP.innerHTML = "Hit space bar to start.";
+    dispP.style.position = "absolute";
+    dispP.style.left = "50%";
+    dispP.style.marginLeft = -(dispP.offsetWidth / 2) + "px";
+}
+
+//Clear start display
+let clearStartDisplay = function(){
+    startDisp.innerHTML="";
+}
+
+setStartDisplay();
+
+//On space bar hit, clear start display, and begin moving snake.
+let startGame = function() {
+    document.addEventListener("keyup", spaceBarHit)
+    function spaceBarHit(){
+        //space bar is 32
+        if(event.keyCode===32){
+            clearStartDisplay();
+            moveSnake();
+        }
+    }
+}
+startGame();
+
+//Set snake direction: e,n,w,s
+let setSnakeDirection = function(dir){
+    snake.direction = dir;
+}
+
+//Assign keyboard controls
+let setControls = function () {
+    document.addEventListener("keyup", arrowHit)
+    function arrowHit(){
+        console.log("key hit")
+        console.log(event.keyCode)
+        switch(event.keyCode){
+            case 39: //east
+            setSnakeDirection("e");
+            break;
+            case 38: //north
+            setSnakeDirection("n");
+            break;
+            case 37: //west
+            setSnakeDirection("w");
+            break;
+            case 40: //south
+            setSnakeDirection("s");
+            break;
+        }
+    }
+}
+
+setControls();
+
+
+
